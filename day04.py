aa32268235d24d547348c7cd705c0c59cee812c3
KimTaeYeong <12170548@inha.edu>
2023-01-17 11:19:44 +0900

    day04_list02

--- day04.py
+++ day04.py
@@ -1,16 +1,24 @@
-# tuple
-a= 'Happy',
-b= ('harry',)
-c= () # empty tuple
-d = 'harry', 'ron'  # packing
-e = ('hermione')  # string
-f = ('harry', 'ron')  # packing
-print(f[1])
-x, y = f  # unpacing
-print(y)
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
-print(type(e))
-print(type(f))
+# list
+# scores = ('B+', 'A+', 'C+')
+# print(scores[1], scores[2])
+# temp = list(scores)
+# temp[1] = 'C+'
+# temp[2] = 'A+'
+# scores = tuple(temp)
+# print(scores[1], scores[2])
+
+big_bang = ['GD', '태양', '탑', '대성', '승리']
+exo = ['백현', '첸']
+big_bang.insert(1, '김박사')
+exo.append(big_bang)
+print(exo)
+print(exo[2][2]) #태양
+exo[-2] = '시우민'
+print(exo[2].pop()) # 승리 pop
+print(exo)
+print(exo[2].pop(-2)) # 탑 pop
+print(exo)
+del exo[2][-1]
+ # 대성 del
+exo[2].remove('김박사')
+print(exo)
