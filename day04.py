238dbb01a237fff6856a06ab5915a1f5a83f8ddb
KimTaeYeong <12170548@inha.edu>
2023-01-17 10:11:11 +0900

    day04_tuple

---
+++ day04.py
@@ -0,0 +1,16 @@
+# tuple
+a= 'Happy',
+b= ('harry',)
+c= () # empty tuple
+d = 'harry', 'ron'  # packing
+e = ('hermione')  # string
+f = ('harry', 'ron')  # packing
+print(f[1])
+x, y = f  # unpacing
+print(y)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
