356d98e1ccfd4f96a0674735e4ac2653bad8d07f
KimTaeYeong <12170548@inha.edu>
2023-01-17 13:42:56 +0900

    day04_list04

--- day04.py
+++ day04.py
@@ -1,33 +1,11 @@
-# list
-import copy
-a = [1, 2, [5, -9]]
-b = a.copy()
-c = list(a)
-d = a[:]
-a[1] = -77 # immutable
-a[2][1] =7  # mutable
-# b = copy.deepcopy(a) #별도의 메모리 할당
-a[2][1] = 7 #mutable, deepcopy
-print(a, b)
-# a = [1, 2, [5, -9]]
-# b = a.copy()
-# c = list(a)
-# d = a[:]
-# a[2][1] = 7 # mutable, b/c/d affects
-# print(a, b, c, d)
-# primes = [2, 19, 3.0, 5, 7, 11]
-# primes_cp = primes
-# print(primes_cp)
-# primes[-1] = 'lunch time'
-# print(primes_cp)
-# primes_cp[0] = 'morning coffee'
-# print(primes_cp)
-# primes = [2, 19, 3.0, 5, 7, 11]
-# print(primes)
-# primes.sort()
-# primes_sorted = sorted(primes)
-# print(primes)
-# #
-# mixed = ['6', '4', '5', 'A', '7', '트와이스', 'B', 'b', '마마무']
-# mixed.sort(reverse=True)
-# print(mixed)
+# list comprehension
+
+odd_lists = [ i for i in range(1, 11) if i % 2 == 1]
+odd_tuple = ( i for i in range(1, 11) if i % 2 == 1)
+# for i in range(1, 11):
+#     if i% 2 ==1 :
+#         odd_lists.append(i)
+# print(odd_lists)
+print(odd_lists)
+print(odd_tuple)
+print(type(odd_tuple) # sd==generator 메모리 거의  저장안함)
